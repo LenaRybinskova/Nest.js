c77b639928c363d45b8d6c6ab9486b6fd5ca0b69
«Elena <LenaRybinskova@gmail.com>
2025-04-29 19:54:15 +0300

    headers decorators

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
-import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [MovieModule, UserModule],
+  imports: [MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
-import { MovieService } from './movie.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Headers,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+/*@Controller({
+  path: 'movie', // это тоже самое что мы так укажем @Controller('movie')
+  host:"api.ru"  // откуда могут запросы приходить, напр с http://api.ru/movie
+})*/
 
 @Controller('movie')
 export class MovieController {
-  constructor(private readonly movieService: MovieService) {}
+  // если 1 парам надо вытащить
+  /*@Get()
+  findAll(@Query("genre") genre:string){
+    return  genre ?`Фильмы в жанре ${genre}`: [{ title: 'lll' }, { title: 'nnn' }]
+  }*/
 
-  @Get('test')
-  async test() {
-    return this.movieService.test();
+  // если несколько парам надо вытащить
+  @Get()
+  findAll2(@Query() queryParams: any) {
+    // вместо эни должно быть ДТО
+    return JSON.stringify(queryParams);
+  }
+
+  @Post()
+  create(@Body('title') title: string) {
+    return `Фильмы ${title} добавлен.`;
+  }
+
+  // вытащить ВСЕ хедерс
+  @Get('headers')
+  getHead(@Headers() headers: any) {
+    return headers;
+  }
+
+  // вытащить конкретно что то из Хедерс
+  @Get('user-agent')
+  getUserAgent(@Headers('user-agent') userAgent: string) {
+    return { userAgent };
+  }
+
+  // вытащить ВСЕ параметры запроса(хедерс, боди.. )
+  @Get('request')
+  getRequestDetails(@Req() req: Request) {
+    return { method: req.method }; // если вернуть чисто req то будет 500 ошибка..это связ с серилизацией сложного объекта req, надо всегда вытягивать конкретные поля
   }
 }
