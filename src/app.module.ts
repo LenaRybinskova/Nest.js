4c7765791e2df986202e5400c8e4df0f62b7231c
«Elena <LenaRybinskova@gmail.com>
2025-05-28 11:52:17 +0300

    LoggingMiddleware global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
@@ -20,9 +25,11 @@ import { LoggingMiddleware } from 'src/common/middlewares/logger.moddleware'
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule{
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes({path:"/movies", method: RequestMethod.POST}) // // конкр машр и тип запроса
+    consumer
+      .apply(LoggingMiddleware)
+      .forRoutes({ path: '/movies', method: RequestMethod.POST }) // // конкр машр и тип запроса
     // consumer.apply(LoggingMiddleware).forRoutes(AppController) // можно указать конкр контроллер
     // consumer.apply(LoggingMiddleware).forRoutes("*")// можно указать ВСЕ маршруты
     // consumer.apply(LoggingMiddleware).exclude() // для каких маршр он не будет сраб
