d76828839f888ffdfe3fe7f514db6a93daf5340d
«Elena <LenaRybinskova@gmail.com>
2025-05-30 11:23:32 +0300

    swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,28 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { logger } from 'src/common/middlewares/logger.moddleware'
-import { AuthGuard } from 'src/common/guards/auth.guard'
-import { ResponseInterseptor } from 'src/common/interseptors/response.interseptor'
 import { AllExeptionsFilter } from 'src/common/filters/all-exeptions.filter'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe()) // добавл для валидации всех запр
-  //app.setGlobalPrefix('api');
-
-  app.use(logger) //подкл функ логирования мидлвер
-  //app.useGlobalGuards(new AuthGuard) //подкл AuthGuard глобально
-  //app.useGlobalInterceptors(new ResponseInterseptor())
   app.useGlobalFilters(new AllExeptionsFilter)
+  app.use(logger) //подкл функ логирования мидлвер
+  //app.setGlobalPrefix('api');
+  //app.useGlobalGuards(new AuthGuard) //подкл AuthGuard глобально
+  //app.useGlobalInterceptors(new ResponseInterseptor()) //подкл интерсепторов глобально
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest js course')
+    .setDescription('Api documentation')
+    .setVersion('1.0.0')
+    .setContact('Lena Rybinskova', 'http://kllfdk', 'support@mail.com')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('/swagger', app, document)
 
   await app.listen(3000)
 }
