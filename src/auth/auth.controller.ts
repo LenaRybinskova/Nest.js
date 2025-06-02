c2fd14189f9d0279675da28faaacf137df7b98fd
«Elena <LenaRybinskova@gmail.com>
2025-06-02 14:44:24 +0300

    base

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { RegisterRequest } from 'src/auth/dto/registerRequest'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED) //201 ответ если все ок
+  async register(@Body() dto: RegisterRequest) {
+    return await this.authService.register(dto)
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { RegisterRequest } from 'src/auth/dto/registerRequest'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaServis: PrismaService) {
+  }
+
+  async register(dto: RegisterRequest) {
+    const { name, password, email } = dto
+
+    //1 проверка может есть такой юзер уже.
+    const existUser = await this.prismaServis.user.findUnique({
+      where: { email },
+    })
+
+    //если юзер такой есть то выбр ошибку
+    if (existUser) {
+      throw new ConflictException('Пользователь с такой почтой уже существует')
+    }
+
+    //2 сохр в БД
+    const user = this.prismaServis.user.create({
+      data: { name, email, password },
+    })
+
+    return user
+  }
+}
