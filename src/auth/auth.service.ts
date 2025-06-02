e06bab191fe6a391872b9be31065fcffe4126514
«Elena <LenaRybinskova@gmail.com>
2025-06-02 17:21:23 +0300

    JWT access refresh tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,36 @@
 import { ConflictException, Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { RegisterRequest } from 'src/auth/dto/registerRequest'
+import { hash } from 'argon2'
+import { ConfigService } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+import { JwtPayload } from 'src/auth/interfaces/jwt.interface'
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaServis: PrismaService) {
+  private readonly JWT_SECRET: string
+  private readonly JWT_ACCESS_TOKEN_TTL: string
+  private readonly JWT_REFRESH_TOKEN_TTL: string
+
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.JWT_SECRET = configService.getOrThrow<string>('JWT_SECRET')
+    this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<string>(
+      'JWT_ACCESS_TOKEN_TTL',
+    )
+    this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<string>(
+      'JWT_REFRESH_TOKEN_TTL',
+    )
   }
 
   async register(dto: RegisterRequest) {
     const { name, password, email } = dto
 
     //1 проверка может есть такой юзер уже.
-    const existUser = await this.prismaServis.user.findUnique({
+    const existUser = await this.prismaService.user.findUnique({
       where: { email },
     })
 
@@ -21,10 +40,23 @@ export class AuthService {
     }
 
     //2 сохр в БД
-    const user = this.prismaServis.user.create({
-      data: { name, email, password },
+    const user = await this.prismaService.user.create({
+      data: { name, email, password: await hash(password) },
+    })
+    return this.generateTokens(user.id)
+  }
+
+  private generateTokens(id: string) {
+    const payload: JwtPayload = { id } //на базе Ид будет создан Пейлод
+
+    const accessToken = this.jwtService.sign(payload, { //Nest генерит аксесс токен с таким сроком жзини
+      expiresIn: this.JWT_ACCESS_TOKEN_TTL,
     })
 
-    return user
+    const refreshToken = this.jwtService.sign(payload, { //Nest генерит рефреш токен с таким сроком жзини
+      expiresIn: this.JWT_REFRESH_TOKEN_TTL,
+    })
+
+    return { accessToken, refreshToken}
   }
 }
--- src/auth/dto/registerRequest.ts
+++ src/auth/dto/registerRequest.ts
@@ -1,5 +1,24 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator'
+
 export class RegisterRequest {
+  @IsString({ message: 'Имя должно быть строкой' })
+  @IsNotEmpty({ message: 'обязательное поле' })
+  @MaxLength(50, { message: 'максимум 50 символов' })
   name: string
+  @IsString({ message: 'Почта должна быть строкой' })
+  @IsNotEmpty({ message: 'обязательное поле' })
+  @IsEmail({}, { message: 'не формат почты' })
   email: string
+
+  @IsString({ message: 'Пароль должна быть строкой' })
+  @IsNotEmpty({ message: 'обязательное поле' })
+  @MinLength(6, { message: 'максимум 6 символов' })
+  @MaxLength(50, { message: 'максимум 50 символов' })
   password: string
 }
